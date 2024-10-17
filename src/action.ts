27a6d0580f9ebf7b619ed05facfac1c9bf603b38
Jonas Strassel <info@jonas-strassel.de>
2024-10-17 10:05:22 +0200

    🎨 Spaces

--- src/action.ts
+++ src/action.ts
@@ -2,7 +2,7 @@ import { debug, exportVariable, getInput } from "@actions/core";
 import { getRPCUrl, networkEnv, supportedChainIds } from "./lib";
 
 const alchemyKey =
-	getInput("ALCHEMY_API_KEY") !== "" ? getInput("ALCHEMY_API_KEY") : undefined;
+  getInput("ALCHEMY_API_KEY") !== "" ? getInput("ALCHEMY_API_KEY") : undefined;
 
 /**
  * Iterates over the supported chain ids and sets the corresponding env var
@@ -10,17 +10,17 @@ const alchemyKey =
  * Tries to use the input first, then constructs the url from the alchemy key.
  */
 for (const chainId of supportedChainIds) {
-	const envVarName = networkEnv[chainId];
+  const envVarName = networkEnv[chainId];
 
-	const input = getInput(envVarName);
+  const input = getInput(envVarName);
 
-	const hasEnvVar = input && input !== "";
+  const hasEnvVar = input && input !== "";
 
-	if (hasEnvVar) {
-		debug(`Found '${envVarName}' env var and using it.`);
-		exportVariable(networkEnv[chainId], input);
-	} else {
-		debug(`No '${envVarName}; env var, using alchemy.`);
-		exportVariable(networkEnv[chainId], getRPCUrl(chainId, alchemyKey));
-	}
+  if (hasEnvVar) {
+    debug(`Found '${envVarName}' env var and using it.`);
+    exportVariable(networkEnv[chainId], input);
+  } else {
+    debug(`No '${envVarName}; env var, using alchemy.`);
+    exportVariable(networkEnv[chainId], getRPCUrl(chainId, alchemyKey));
+  }
 }
