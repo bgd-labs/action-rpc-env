cd4ea52c62bf054252840745e63566cf97cbe99d
Lukas <ls@bgdlabs.com>
2024-11-26 14:00:30 +0100

    fix: add default quicknode (#45)

--- src/action.ts
+++ src/action.ts
@@ -4,6 +4,16 @@ import { ChainId, getNetworkEnv, getRPCUrl } from "./lib";
 const alchemyKey =
   getInput("ALCHEMY_API_KEY") !== "" ? getInput("ALCHEMY_API_KEY") : undefined;
 
+const quicknodeToken =
+  getInput("QUICKNODE_TOKEN") !== ""
+    ? getInput("QUICKNODE_TOKEN")
+    : "5196b99cdae04535ecd62906c3b618876686618a";
+
+const quicknodeEndpointName =
+  getInput("QUICKNODE_ENDPOINT_NAME") !== ""
+    ? getInput("QUICKNODE_ENDPOINT_NAME")
+    : "quaint-still-liquid";
+
 /**
  * Iterates over the supported chain ids and sets the corresponding env var
  * if the input is not empty.
@@ -12,5 +22,8 @@ const alchemyKey =
 for (const chainId of Object.values(ChainId)) {
   const envVarName = getNetworkEnv(chainId);
   debug(`Setting ${envVarName}`);
-  exportVariable(envVarName, getRPCUrl(chainId, { alchemyKey: alchemyKey }));
+  exportVariable(
+    envVarName,
+    getRPCUrl(chainId, { alchemyKey, quicknodeToken, quicknodeEndpointName }),
+  );
 }
