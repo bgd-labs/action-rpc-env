bd44393dfc87484624490e2c108d64fccda071c6
Lukas <ls@bgdlabs.com>
2025-03-24 19:56:37 +0100

    fix: update public rpcs (#91)

--- src/publicRPCs.ts
+++ src/publicRPCs.ts
@@ -7,7 +7,7 @@ import { ChainId } from "./chainIds";
 export const publicRPCs = {
   [ChainId.mainnet]: "https://eth.llamarpc.com",
   [ChainId.polygon]: "https://polygon.llamarpc.com",
-  [ChainId.arbitrum]: "https://polygon.llamarpc.com",
+  [ChainId.arbitrum]: "https://arbitrum.llamarpc.com",
   [ChainId.base]: "https://base.llamarpc.com",
   [ChainId.bnb]: "https://binance.llamarpc.com",
   [ChainId.metis]: "https://andromeda.metis.io/?owner=1088",
